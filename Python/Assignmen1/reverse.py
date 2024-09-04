0959f898f1aeb31933cd59872f7654461534de5f
Muhammad Saqib <muhammad.saqib@eurustechnologies.com>
2024-09-04 17:07:50 +0500

    adding python question 4

---
+++ Python/Assignmen1/reverse.py
@@ -0,0 +1,29 @@
+def custom_length(word): 
+    count = 0
+    for letter in word:
+        count += 1
+    return count
+
+def custom_validation(number):
+    while True:
+        valid = True
+        for char in number:
+            if not ('0' <= char <= '9'):
+                valid = False
+                break
+        
+        if valid:
+            return number
+        else:
+            input("Please enter a valid number again ")
+
+def reverse_digits(number):
+    index = custom_length(number) - 1 
+    print("Revser order for ", number , " is :" , end=" " )
+    while(index>=0):
+        print(number[index] , end=" ")
+        index-=1
+    print()
+    
+number = custom_validation(input("Enter a number: "))
+reverse_digits(number)
--- Python/Assignmen1/test_substring.py
+++
@@ -1,9 +0,0 @@
-
-
-sub_string="value"
-s="Saqibmy value is"
-index = 0
-word_length = 5
-
-print(s[index:index + word_length])
-
