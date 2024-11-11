4d7851e8376a2b58fc0f58fe0014f590728c4538
Muhammad Saqib <muhammad.saqib@eurustechnologies.com>
2024-11-11 19:53:42 +0500

    Update main.tf

--- IaCTasks/user_iden_with_role_assignment/modules/user_identities_with_role_assignments/main.tf
+++ IaCTasks/user_iden_with_role_assignment/modules/user_identities_with_role_assignments/main.tf
@@ -4,6 +4,8 @@ resource "azurerm_user_assigned_identity" "example" {
   resource_group_name = var.resource_group_name
 }
 
+
+
 output "identity" {
   value = azurerm_user_assigned_identity.example
 }
