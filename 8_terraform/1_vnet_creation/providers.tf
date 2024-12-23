372353325eeb7a9eca819d1b8de3ef33f282cec1
Muhammad Saqib <muhammad.saqib@eurustechnologies.com>
2024-12-23 12:21:51 +0500

    Update providers.tf

--- 8_terraform/1_vnet_creation/providers.tf
+++ 8_terraform/1_vnet_creation/providers.tf
@@ -2,15 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = "=4.0.0"
     }
   }
 }
 
 provider "azurerm" {
-    # subscription_id = var.subscription_id
-    # client_secret   = var.client_secret
-    # client_id       = var.client_id
-    # tenant_id       = var.tenant_id
+
     features {}
 }
